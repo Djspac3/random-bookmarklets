98666d327e50e11cc13b83049a99d3dcf7e23a33
Djspac3 <149281216+Djspac3@users.noreply.github.com>
2025-06-24 17:21:22 +0000

    changed setup to just clear the document

--- src/setup.js
+++ src/setup.js
@@ -1,25 +1,6 @@
-import './style.css'
-import logo from './logo.svg'
-
 export default function main() {
     alert('Bookmarklet Manager is starting up...')
     /* remove all unneeded elements from existance */
     document.body.innerHTML = ''
     document.head.innerHTML = ''
-
-    /* start of real script */
-    var favIcon = document.createElement('link')
-    favIcon.rel = 'icon'
-    favIcon.href = logo
-    favIcon.type = 'image/svg+xml'
-
-    var style = document.createElement('style')
-    style.innerHTML = mainStyle
-
-    var bookmarklist = document.createElement('div')
-    bookmarklist.id = 'bookmarklist'
-
-    /* append every element here! */
-    document.head.append(style,favIcon)
-    document.body.append(bookmarklist)
 }

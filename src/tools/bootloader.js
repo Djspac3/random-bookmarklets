d5d8dc421f52fb943cb4a785dba6341bdde75047
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-12 17:16:14 +0000

    add tools

---
+++ src/tools/bootloader.js
@@ -0,0 +1,20 @@
+
+var option = Number(prompt(
+    `Choose an option (type number of it):
+    1. devtools (eruda)
+    ...`
+));
+switch (option) {
+    case 1:
+        const script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/eruda';
+        script.onload = () => {
+            eruda.init();
+        };
+        document.head.appendChild(script);
+        break;
+    // Add more cases for other options as needed
+    default:
+        alert('Invalid option. Please try again.');
+        break;
+}

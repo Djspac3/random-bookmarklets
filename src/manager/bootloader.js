2a2bbe3fd98592263e528689e5b080c5bea408c2
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-13 16:48:22 +0000

    small formating change

--- src/manager/bootloader.js
+++ src/manager/bootloader.js
@@ -1,7 +1,7 @@
 import {addElements as setupPhase, main} from './main.js'; // Import the page clearer script
 
 if (!window.location.href.startsWith('https://google.com/bookmarlet.manager')) {
-    console.error('Bookmarklet is not running on the correct page. Redirecting to the main site.');
+    console.error('Bookmarklet manager is not running on the correct page. Redirecting to the main site.');
     alert('This bookmarklet is designed to be used on https://google.com/bookmarlet.manager for page reasons. you will be redirected now. rerun this script after the redirect.');
     window.location.href = "https://google.com/bookmarlet.manager" // the main site
 }

0cc2ab21447d074a809861a6efdafbffab36d827
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-14 17:43:47 +0000

    export logs,warnings,and errors in bootloader for testing

--- src/manager/bootloader.js
+++ src/manager/bootloader.js
@@ -1,7 +1,18 @@
 import {addElements as setupPhase, main} from './main.js'; // Import the page clearer script
 
+// values for tests
+var logs = {
+
+}
+var warns = {
+
+}
+var errors = {
+    'invalidUrl': 'Bookmarklet manager: invalid URL, redirecting to the base url',
+} 
+
 if (!window.location.href.startsWith('https://google.com/bookmarlet.manager')) {
-    console.error('Bookmarklet manager: invalid URL, redirecting to the base url');
+    console.error(errors.invalidUrl);
     alert('This bookmarklet is designed to be used on https://google.com/bookmarlet.manager for page reasons. you will be redirected now. rerun this script after the redirect.');
     window.location.href = "https://google.com/bookmarlet.manager" // the main site
 }
@@ -13,6 +24,4 @@ setupPhase(); // Call the setup function to add elements to the page
 
 main(); // Call the main function to initialize the page
 
-// values for errors blah blah blah
-
-//reminder to self do this later due to sleep needed
+export {logs, warns, errors}; // Export the logs, warns, and errors for testing purposes

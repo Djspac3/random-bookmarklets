80beab9243acc6065b0b97d8e5f712aa6ef7fc3f
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-14 02:08:29 +0000

    unfinished stuff

--- src/manager/bootloader.js
+++ src/manager/bootloader.js
@@ -1,7 +1,7 @@
 import {addElements as setupPhase, main} from './main.js'; // Import the page clearer script
 
 if (!window.location.href.startsWith('https://google.com/bookmarlet.manager')) {
-    console.error('Bookmarklet manager is not running on the correct page. Redirecting to the main site.');
+    console.error('Bookmarklet manager: invalid URL, redirecting to the base url');
     alert('This bookmarklet is designed to be used on https://google.com/bookmarlet.manager for page reasons. you will be redirected now. rerun this script after the redirect.');
     window.location.href = "https://google.com/bookmarlet.manager" // the main site
 }
@@ -12,3 +12,7 @@ document.body.innerHTML = ''; // Clear the body
 setupPhase(); // Call the setup function to add elements to the page
 
 main(); // Call the main function to initialize the page
+
+// values for errors blah blah blah
+
+//reminder to self do this later due to sleep needed
--- tests/run.scripts.test.ts
+++ tests/run.scripts.test.ts
@@ -1,18 +1,48 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import * as fs from "fs";
+import path from "path";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const bookmarkletDir: fs.PathLike = path.join(__dirname, "..", "bookmarklets");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+// Read all bookmarklet files
+if (!fs.existsSync(bookmarkletDir)) {
+  throw new Error(
+    `Bookmarklets directory does not exist please run the build script first.`
+  );
+}
+if (!fs.lstatSync(bookmarkletDir).isDirectory()) {
+  throw new Error(`Bookmarklets path is not a directory.`);
+}
+const bookmarkletFiles = fs
+  .readdirSync(bookmarkletDir)
+  .filter((file) => file.endsWith(".js"));
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("Bookmarklets", async () => {
+  for (const bookmarklet of bookmarkletFiles) {
+    const bookmarkletPath = path.join(bookmarkletDir, bookmarklet);
+    const bookmarkletContent = fs.readFileSync(bookmarkletPath);
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    
+    switch (bookmarklet) {
+      case "manager.js":
+        test("Bookmarklet: manager.js, invalid page", async ({ page }) => {
+          await page.goto("https://google.com/no.page");
+          await page.addScriptTag({ content: bookmarkletContent });
+          // Add assertions or interactions as needed
+          page.on("console", (msg) => {
+            if (msg.type() === "error") {
+              // check if error is correctly handled
+              expect(msg.text()).toContain(
+                "Bookmarklet manager: invalid URL"
+              );
+            }
+          });
+        });
+        test("Bookmarklet: manager.js, valid page", async ({ page }) => {
+          await page.goto("https://google.com/bookmarlet.manager");
+        });
+        break;
+      default:
+    }
+  }
 });

5becf437d1d1159a586f720f07d1eaffa00c42db
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-29 00:33:08 +0000

    remove anoying secondarly describe thats useless

--- tests/run.scripts.test.ts
+++ tests/run.scripts.test.ts
@@ -17,60 +17,61 @@ const bookmarkletFiles = fs
   .readdirSync(bookmarkletDir)
   .filter((file) => file.endsWith(".js"));
 
-test.describe("Bookmarklets", async () => {
-  for (const bookmarklet of bookmarkletFiles) {
-    const bookmarkletPath = path.join(bookmarkletDir, bookmarklet);
-    const bookmarkletContent = fs.readFileSync(bookmarkletPath);
+for (const bookmarklet of bookmarkletFiles) {
+  const bookmarkletPath = path.join(bookmarkletDir, bookmarklet);
+  const bookmarkletContent = fs.readFileSync(bookmarkletPath);
 
-    
-    switch (bookmarklet) {
-      case "manager.js":
-        test("Bookmarklet: manager.js, invalid page", async ({ page }) => {
-          await page.goto("https://google.com/no.page");
-          page.addScriptTag({content: bookmarkletContent.toString()});
+  
+  switch (bookmarklet) {
+    case "manager.js":
+      test("Bookmarklet: manager.js, invalid page", async ({ page }) => {
+        await page.goto("https://google.com/no.page");
+        page.addScriptTag({content: bookmarkletContent.toString()});
 
-          var consoleMessages: ConsoleMessage[] = [];
+        var consoleMessages: ConsoleMessage[] = [];
           page.on("console", (msg) => {
-            if (msg.type() === "error") {
-              // check if error is correctly handled
-              consoleMessages.push(msg);
-            }
-          });
-          // wait for it to finish
-          await page.waitForTimeout(1000);
-          expect(consoleMessages.length).toBeGreaterThan(0);
-          expect(consoleMessages[0].type()).toBe("error");
-          expect(consoleMessages[0].text()).toContain("bookmarklet manager: unvalid URL");
-        });
-        test("Bookmarklet: manager.js, valid page", async ({ page }) => {
-          await page.goto("https://google.com/bookmarlet.manager");
-          page.addScriptTag({content: bookmarkletContent.toString()});
-
-          var consoleMessages: ConsoleMessage[] = [];
-          page.on("console", (msg) => {
+          if (msg.type() === "error") {
+            // check if error is correctly handled
             consoleMessages.push(msg);
-          });
-
-          await page.waitForTimeout(1000);
-          
+          }
         });
-        break;
-      default:
-        test(`Bookmarklet: ${bookmarklet}, unknown script for testing, checking for errors`, async ({ page }) => {
-          await page.goto(`https://google.com/bookmarlet.${bookmarklet}`);
-          page.addScriptTag({content: bookmarkletContent.toString()});
+        // wait for it to finish
+        await page.waitForTimeout(1000);
+        expect(consoleMessages.length).toBeGreaterThan(0);
+        expect(consoleMessages[0].type()).toBe("error");
+        expect(consoleMessages[0].text()).toContain("bookmarklet manager: unvalid URL");
+      });
+      test("Bookmarklet: manager.js, valid page", async ({ page }) => {
+        await page.goto("https://google.com/bookmarlet.manager");
+        page.addScriptTag({content: bookmarkletContent.toString()});
 
-          var consoleMessages: ConsoleMessage[] = [];
-          page.on("console", (msg) => {
-            consoleMessages.push(msg);
-          });
-
-          await page.waitForTimeout(1000);
-          // Check if the bootloader logs are present
-          expect(consoleMessages.length).toBeGreaterThan(0);
-          expect(consoleMessages[0].text()).toContain("Bookmarklet loaded successfully");
+        var consoleMessages: ConsoleMessage[] = [];
+        page.on("console", (msg) => {
+          consoleMessages.push(msg);
         });
-        break;
-    }
+
+        await page.waitForTimeout(1000);
+        
+      });
+      break;
+    case "tools.js":
+      
+      break;
+    default:
+      test(`Bookmarklet: ${bookmarklet}, unknown script for testing, checking for errors`, async ({ page }) => {
+        await page.goto(`https://google.com/bookmarlet.${bookmarklet}`);
+        page.addScriptTag({content: bookmarkletContent.toString()});
+
+        var consoleMessages: ConsoleMessage[] = [];
+        page.on("console", (msg) => {
+          consoleMessages.push(msg);
+        });
+
+        await page.waitForTimeout(1000);
+        // Check if the bootloader logs are present
+        expect(consoleMessages.length).toBeGreaterThan(0);
+        expect(consoleMessages[0].text()).toContain("Bookmarklet loaded successfully");
+      });
+      break;
   }
-});
+};

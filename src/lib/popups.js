81b44b1345ffe06431870c52b76d41b83caddb8d
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-29 01:01:37 +0000

    add unfinished popup library with only one used function

---
+++ src/lib/popups.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number?} width positive integer for popup width
+ * @param {number?} height positive integer for popup height
+ * @returns {WindowProxy} popup window
+ */
+export function promptHtml(width, height) {
+  width = width || 600;
+  height = height || 400;
+  html = html || "<html><body>NO HTML USED</body></html>";
+  var popup = open(
+    "about:blank",
+    "",
+    "prompt=true,width=" +
+      width +
+      ",height=" +
+      height +
+      ",resizable=yes,scrollbars=yes"
+  );
+  if (popup) {
+    callback = callback || function () {};
+    callback(popup);
+  } else {
+    alert("Popup blocked! Please allow popups for this site.");
+    throw new Error("library: promptHtml: Popup blocked");
+  }
+  return popup;
+}

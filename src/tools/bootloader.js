3ca0976280566a6f0cb25d494d3301eae2130bdd
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-29 17:00:00 +0000

    formatting and using library for prompting

--- src/tools/bootloader.js
+++ src/tools/bootloader.js
@@ -1,20 +1,25 @@
-
-var option = Number(prompt(
+import { prompt } from "../lib/popups";
+var option = Number(
+  prompt(
     `Choose an option (type number of it):
     1. devtools (eruda)
-    ...`
-));
+    ...`,
+    "1", // default value
+    true, //retry
+    "number" // type
+  )
+);
 switch (option) {
-    case 1:
-        const script = document.createElement('script');
-        script.src = 'https://cdn.jsdelivr.net/npm/eruda';
-        script.onload = () => {
-            eruda.init();
-        };
-        document.head.appendChild(script);
-        break;
-    // Add more cases for other options as needed
-    default:
-        alert('Invalid option. Please try again.');
-        break;
+  case 1:
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/eruda";
+    script.onload = () => {
+      eruda.init();
+    };
+    document.head.appendChild(script);
+    break;
+  // Add more cases for other options as needed
+  default:
+    alert("Invalid option. Please try again.");
+    break;
 }

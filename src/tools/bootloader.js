14cf4c5f6eb7438550f237cce3e3c2f9527e30f6
DJ space <149281216+Djspac3@users.noreply.github.com>
2025-07-31 16:35:37 +0000

    update tools formating and use parseFloat for numbers ig

--- src/tools/bootloader.js
+++ src/tools/bootloader.js
@@ -1,4 +1,5 @@
 //import { prompt } from "../lib/popups";
+
 var option = Number(
   prompt(
     `Choose an option (type number of it):
@@ -7,13 +8,18 @@ var option = Number(
     "1"
   )
 );
-switch (option) {
-  case "1":
+switch (parseFloat(option)) {
+  case 1:
     const script = document.createElement("script");
+
+    // TODO: make this use either a built in copy of eruda or bypass CORS and use the CDN
     script.src = "https://cdn.jsdelivr.net/npm/eruda";
     script.onload = () => {
       eruda.init();
     };
+    script.onerror = (error) => {
+      alert(`Failed to load eruda. error: ${error || "Unknown error"}`);
+    }
     document.head.appendChild(script);
     break;
   // Add more cases for other options as needed

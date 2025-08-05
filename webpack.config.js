f12cfdba197ac956b77278a336b91f0e9e31d86a
dj friedman <buildingwithdj+git@gmail.com>
2025-08-05 08:34:57 -0400

    make bundle analyzer NOT always open

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // makes it output a html file with all the data!
       reportFilename: "bundleSizes.html",
+      openAnalyzer: false
     }),
   ],
 };

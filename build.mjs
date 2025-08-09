da91ec2df913a93fc21e99924d37e699aff6909a
dj friedman <buildingwithdj+git@gmail.com>
2025-08-09 19:40:56 -0400

    add watch to build script

--- build.mjs
+++ build.mjs
@@ -1,18 +1,44 @@
 import * as esbuild from "esbuild";
 import { argv } from "process";
 
-esbuild
-  .build({
-    entryPoints: ["src/bookmarklet-manager/main.jsx"],
-    bundle: true,
-    minify: true,
-    outfile: "bookmarklets/manager.js", // Output file
-    // loaders
-    loader: {
-      ".svg": "base64",
-    },
-  })
-  .then(() => {
-    console.log("Bookmarklet bundled successfully!");
-  })
-  .catch(() => process.exit(1));
+/**@type {import('esbuild').BuildOptions} */
+const config = {
+  entryPoints: ["src/bookmarklet-manager/main.jsx"],
+  bundle: true,
+  minify: true,
+  jsxImportSource: "preact",
+  jsx: "automatic",
+  outfile: "bookmarklets/manager.js", // Output file
+  // loaders
+  loader: {
+    ".svg": "dataurl",
+  },
+  format: "iife",
+};
+
+console.log(argv[2]);
+
+if (argv[2] === "build") {
+  esbuild
+    .build(config)
+    .then(() => {
+      console.log("Bookmarklet bundled successfully!");
+    })
+    .catch(() => process.exit(1));
+}
+if (argv[2] === "watch") {
+  var context = await esbuild.context(config);
+  context.watch();
+  var wait = true;
+  process.on("SIGINT", async () => {
+    console.log("signal interupt noticed, closing esbuild context");
+    await context.dispose();
+    console.log("esbuild context has been 'disposed' (shutdown?)");
+    wait = false;
+  });
+  setInterval(() => {
+    if (!wait) {
+      process.exit();
+    }
+  });
+}
